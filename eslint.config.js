81e11bfb26f2824a010dcdece0b4a35272a8de3d
omitbajracharya <omitbajracharya@gmail.com>
2024-06-10 15:50:17 +0545

    fix: done

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ module.exports = tseslint.config(
           style: "kebab-case",
         },
       ],
-      "@typescript-eslint/no-inferrable-types": "off"
+      "@typescript-eslint/no-inferrable-types": "off",
     },
   },
   {
